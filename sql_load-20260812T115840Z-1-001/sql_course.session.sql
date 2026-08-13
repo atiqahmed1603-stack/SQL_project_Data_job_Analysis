24fc4ed1cb5f1068f49988bf8baeceb5a6c3fcad
atiqahmed1603-stack <atiq.ahmed1603@gmail.com>
2026-08-13 18:28:01 +0530

    Initial SQL project

---
+++ sql_load-20260812T115840Z-1-001/sql_course.session.sql
@@ -0,0 +1,28 @@
+
+SELECT
+job_title_short,
+job_location,
+job_via,
+job_posted_date ::date,
+salary_year_avg
+
+FROM(
+SELECT *
+FROM january_jobs
+UNION ALL
+
+SELECT *
+FROM february_jobs
+
+UNION ALL
+
+SELECT *
+FROM march_jobs)
+AS quarter_job_postings
+WHERE quarter_job_postings.salary_year_avg >70000 AND
+quarter_job_postings.job_title_short = 'Data Analyst'
+ORDER BY quarter_job_postings.salary_year_avg DESC
+
+
+
+
